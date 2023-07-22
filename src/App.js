fa216900f0ee7e774e2778ef555c6982ea87cd51
AnnaFYZ <113105718+AnnaFYZ@users.noreply.github.com>
2023-07-22 16:00:33 +0100

    input values

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+
+function Form (){
+  let [nameValue, setNameValue] = useState("");
+  let [textValue, setTextValue] = useState("");
+  
+  return (
+    <div>
+      <head>
+        <title>Welcome to CYF Chat</title>
+      </head>
+      <body>
+        <h1>CYF Chat</h1>
+        <h2>Send a message</h2>
+        <form action="/messages" method="post">
+          <p>
+            Name:{" "}
+            <input
+              type="text"
+              name="from"
+              placeholder="Your Name"
+              value={nameValue}
+              onChange={(e) => setNameValue(e.target.value)}
+            />{" "}
+            <br />
+            Message:{" "}
+            <input
+              type="text"
+              name="text"
+              placeholder="The message..."
+              value={textValue} onChange={(e) => setTextValue(e.target.value)}
+            />
+            <br />
+          </p>
+          <button type="submit">Send</button>
+        </form>
+
+        <a href="/messages">See all messages</a>
+      </body>
+    </div>
+  );
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Form />
     </div>
   );
 }
